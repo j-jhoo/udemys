d554c0719ff73bd7999a5cd3486b7778c82a33f9
정지후 <loveadsadf1976@gmail.com>
2022-01-18 20:01:14 +0900

    96.사용자 컴포넌트 추가하기

--- section8/public/components/Users/AddUser.js
+++ section8/public/components/Users/AddUser.js
@@ -1,9 +1,19 @@
 import React from "react";
 
 const AddUser = (props) => {
-  <form>
-    <label></label>
-  </form>;
+  const addUserHandler = (event) => {
+    event.preventDefault();
+  };
+
+  return (
+    <form onSubmit={addUserHandler}>
+      <label htmlFor="username">Username</label>
+      <input id="username" type="text" />
+      <label htmlFor="age">Age(years)</label>
+      <input id="age " type="number " />
+      <button type="submit">Add User </button>
+    </form>
+  );
 };
 
 export default AddUser;
