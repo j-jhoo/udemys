bbc9ce144cc7c99617c533a85a85b4d5bbe8dbc2
정지후 <loveadsadf1976@gmail.com>
2022-01-19 00:27:56 +0900

    101.사용자 목록컴포넌트  추가하기 완료

--- section8/src/App.js
+++ section8/src/App.js
@@ -1,10 +1,12 @@
 import React from "react";
 import AddUser from "../public/components/Users/AddUser";
+import UserList from "../public/components/Users/UserList";
 
 function App() {
   return (
     <div>
       <AddUser />
+      <UserList users={[]} />
     </div>
   );
 }
