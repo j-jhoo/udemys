1ffd8a965b13765ab8bbfd1ec7e6a0931a3b912f
정지후 <loveadsadf1976@gmail.com>
2022-01-18 20:32:23 +0900

    97.재사용 card컴포넌트 추가

--- section8/public/components/UI/Card.js
+++ section8/public/components/UI/Card.js
@@ -2,7 +2,9 @@ import React from "react";
 import classes from "./Card.module.css";
 
 const Card = (props) => {
-  return <div className={classes.card}>{props.Children}</div>;
+  return (
+    <div className={`${classes.card} ${props.className}`}>{props.Children}</div>
+  );
 };
 
 export default Card;
