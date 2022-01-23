6e878bfc72f15b1a5a72426245e5caef51b2dee1
정지후 <loveadsadf1976@gmail.com>
2022-01-23 23:29:44 +0900

    useEffect 문법

--- section10-1/src/components/Login/Login.js
+++ section10-1/src/components/Login/Login.js
@@ -11,6 +11,8 @@ const Login = (props) => {
   const [passwordIsValid, setPasswordIsValid] = useState();
   const [formIsValid, setFormIsValid] = useState(false);
 
+  useEffect(() => {}, []);
+
   const emailChangeHandler = (event) => {
     setEnteredEmail(event.target.value);
 
