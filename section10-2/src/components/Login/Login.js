cb452c70675daf2f0e76a9571761bdaebcb2a683
정지후 <loveadsadf1976@gmail.com>
2022-01-29 22:52:55 +0900

    설명 추가

--- section10-2/src/components/Login/Login.js
+++ section10-2/src/components/Login/Login.js
@@ -11,6 +11,7 @@ const emailReducer = (state, action) => {
   // 액션을 다루는 추가
   // 액션으로 디스패치 한 것은 개체
   // 해당 개체는 우형필드를 가진다. => action.type로 유형 필드에 저장된 값이 USER_INPUT과 맞는지 확인해본다.
+  // 두 값을 모두 업데이트하며,
 
   return { value: "", isValid: false };
 };
