5e208e9e78314828077156dfe6ea85afe6567c1b
정지후 <loveadsadf1976@gmail.com>
2022-02-03 23:08:09 +0900

    effect의 dependency로 props를 가질 수 있다

--- section10-2/src/components/Login/Login.js
+++ section10-2/src/components/Login/Login.js
@@ -73,6 +73,7 @@ const Login = (props) => {
   const {isValid: passwordIsValid} = passwordState;
   // 에일리어스 할당 => 값을 할당하는것이 아니라 에일리어스를 할당하는 것 
   // 등호 왼편에서 꺽인 괄호를 사용할때 자동으로 사용하는 문법인 디스트럭쳐링 문법의 일부
+  // effect의 dependency로 props를 가질 수 있다. 
 
 
   useEffect(() => {
