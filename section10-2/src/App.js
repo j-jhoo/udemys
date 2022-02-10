f5ee3dd00adcd95bfc43d3311d1ea6cd8e040184
정지후 <loveadsadf1976@gmail.com>
2022-02-10 23:15:35 +0900

    auth-context wrapp하는 방법

--- section10-2/src/App.js
+++ section10-2/src/App.js
@@ -2,6 +2,7 @@ import React, { useState, useEffect } from "react";
 
 import Login from "./components/Login/Login";
 import Home from "./components/Home/Home";
+import AuthContext from "./store/auth-context";
 import MainHeader from "./components/MainHeader/MainHeader";
 
 function App() {
@@ -29,11 +30,14 @@ function App() {
 
   return (
     <React.Fragment>
-      <MainHeader isAuthenticated={isLoggedIn} onLogout={logoutHandler} />
-      <main>
-        {!isLoggedIn && <Login onLogin={loginHandler} />}
-        {isLoggedIn && <Home onLogout={logoutHandler} />}
-      </main>
+      <AuthContext.Provider>
+        {/* 점을 찍으면 컴포넌트를 포함하고 있는 객체 상의 특징을 볼 수 있고,그것이 공급자이다.  */}
+        <MainHeader isAuthenticated={isLoggedIn} onLogout={logoutHandler} />
+        <main>
+          {!isLoggedIn && <Login onLogin={loginHandler} />}
+          {isLoggedIn && <Home onLogout={logoutHandler} />}
+        </main>
+      </AuthContext.Provider>
     </React.Fragment>
   );
 }
