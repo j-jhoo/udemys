9caf4133af3e107bf63c5b53b4cba4f83a16df02
정지후 <loveadsadf1976@gmail.com>
2022-02-10 23:12:59 +0900

    context의 첫번째 : 공급

--- section10-2/src/store/auth-context.js
+++ section10-2/src/store/auth-context.js
@@ -8,3 +8,8 @@ const AuthContext = React.createContext({
 // createContext는 디폴트 context를 가지고 이 context는 나의 앱이나 컴포넌트 while state이다.
 
 export default AuthContext;
+
+// * 공급은 항상 첫 번째 단계
+// * 공급은 jsx코드에서 wrap하는 것을 뜻하다.
+// * 모든 컴포넌트 내에서 context를 들을 수 있어야 한다.
+// * !! wrap되지 않은 컴포넌트는 들을 수 없다.
