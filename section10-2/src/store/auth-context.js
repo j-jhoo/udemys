58b69dd0aa466257c9fdeff46bbf74d8ae50c5f6
정지후 <loveadsadf1976@gmail.com>
2022-02-10 23:09:40 +0900

    auth-context파일생성

--- section10-2/src/store/auth-context.js
+++ section10-2/src/store/auth-context.js
@@ -1,6 +1,10 @@
 import react from "react";
 
-React.createContext();
+const AuthContext = React.createContext({
+  isLoggedIn: false,
+});
 // react 대신에 createContext요청
 // 마지막에는 객체를 생성
 // createContext는 디폴트 context를 가지고 이 context는 나의 앱이나 컴포넌트 while state이다.
+
+export default AuthContext;
